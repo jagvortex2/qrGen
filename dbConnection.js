b17b318bccf2c04036f643e51841e50d3793d5a8
Zeke16 <kr2000.16@gmail.com>
2024-01-02 10:58:10 -0600

    Generar json y crear archivo  en disco

--- dbConnection.js
+++ dbConnection.js
@@ -1,17 +1,22 @@
 import sql from 'mssql';
+import fs, { writeFile } from "fs";
+
+
+let result;
+let jsonResults;
+const path = './assets/registro_detalleRegistro.json';
 
 //Funcion utilizada para crear una configuracion y conexion a la base de datos a ser utilizada para ver registros
 function  dbConnect() {
 
-    let result;
-    let jsonResults;
+    
 
     // configurar la base de datos
     const config = {
         user: 'sa',
         password: '$0ftland',
-        server: 'localhost',
-        database: 'BODEGA_NYC',
+        server: '192.168.0.44',
+        database: 'BODEGA',
         synchronize: true,
         trustServerCertificate: true,
     };
@@ -26,21 +31,29 @@ function  dbConnect() {
                 result = await sql.query('SELECT ArticuloDetalle, PrecioUnitario, Descripcion, Articulo, CodigoBarra, EmpresaDestino, Cantidad ' +
                     'FROM BODEGA.dbo.REGISTRO INNER JOIN BODEGA.dbo.DETALLEREGISTRO' +
                     ' ON BODEGA.dbo.REGISTRO.IdRegistro = BODEGA.dbo.DETALLEREGISTRO.IdRegistro;');
-                console.log(result);
+               // console.log(result);
                 const records = result.recordset;
 
             // Imprime el resultado como JSON
             jsonResults =JSON.stringify(records, null, 2);
-            console.log(jsonResults);
+            //console.log(jsonResults);
 
 
             // Itera sobre los registros
             records.forEach(record => {
                 console.log(record); // Aquí puedes acceder a las columnas de cada registro
+    
+            });
+            fs.writeFile(path, jsonResults, (error) => {
+                if(error){
+                    console.log('An error has ocurred trying to write the json file to disk', error);
+                    return;
+                }
+                else console.log('Data written succesfully to disk!');
             });
 
             }catch(err){
-                console.log("Error trying to connect to database:" + err.message());
+                console.log("Error trying to connect to database:" + err.message);
             } finally {
                 sql.close();
                 console.log("Connection to Db Closed succesfully");
@@ -48,10 +61,13 @@ function  dbConnect() {
         };
 
     connection();
-    return jsonResults;
+    
 }
 
 
 
- //dbConnect();
+  dbConnect();
+ 
+ 
+
 export default dbConnect;
--- qrCodeGenerator.js
+++ qrCodeGenerator.js
@@ -7,9 +7,17 @@ import dbConnect from "./dbConnection.js";
  */
 const data = dbConnect();    //Se obtiene la informacion a ser convertida en qr code
 
+
+
 data.forEach(record => {
     if (record.EmpresaDestino === 'cany');
     console.log(record); // Aquí puedes acceder a las columnas de cada registro
 });
 
 
+QRCode.toFile('./images/file.png', 'Encode this text in QR code', {
+    errorCorrectionLevel: 'H'
+  }, function(err) {
+    if (err) throw err;
+    console.log('QR code saved!');
+  });
